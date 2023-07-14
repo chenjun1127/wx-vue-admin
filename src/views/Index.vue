421cb28d17c0d7661c0bd4bc3edae5461813de4c
chenjun <chenjun@orvibo.com>
2023-07-14 17:43:18 +0800

    update style

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,14 +1,33 @@
-
-
-
 <template>
-  <TopMenu></TopMenu> 
-  <main>
-    <div class="left">111</div>
-    <RouterView></RouterView>
+  <main class="main">
+    <aside>
+      <TopMenu></TopMenu>
+    </aside>
+    <section>
+      <RouterView></RouterView>
+    </section>
   </main>
 </template>
  
 <script lang="ts" setup>
 import TopMenu from '../components/TopMenu.vue';
 </script>
+<style lang="scss" scoped>
+.main {
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  height: 100vh;
+
+  aside {
+    width: 230px;
+    transition: transform 0.3s ease-in-out, width 0.3s ease-in-out;
+    height: 100vh;
+    background-color: #222d32;
+  }
+
+  section {
+    flex: auto;
+  }
+}
+</style>
